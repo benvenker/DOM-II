f86fe52f50d7f0a44825b0079aa97b2554e431ea
Ben Venker <bvemails@gmail.com>
2020-03-03 11:43:25 -0800

    finish events

--- js/index.js
+++ js/index.js
@@ -5,8 +5,22 @@ logoHeading.addEventListener("mouseover", e => {
 });
 
 const body = document.querySelector("body");
-body.addEventListener("keydown", () => {
-  alert("How can we help you?");
+body.addEventListener("keydown", e => {
+  if (e.keyCode === 72) {
+    alert("How can we help you?");
+  }
+});
+
+window.addEventListener("beforeprint", () => {
+  alert("are you sure you want to print this page?");
+});
+
+window.addEventListener("afterprint", () => {
+  body.style.backgroundColor = "blue";
+});
+
+window.addEventListener("resize", () => {
+  body.style.backgroundColor = "lavender";
 });
 
 const hero = document.querySelector(".intro img");
@@ -21,4 +35,17 @@ hero.addEventListener("mouseleave", () => {
 const letsGoImg = document.querySelector(".img-content img");
 letsGoImg.addEventListener("load", () => {
   letsGoImg.classList.add("change");
+});
+
+const adventureImg = document.querySelector(".inverse-content img");
+adventureImg.addEventListener("mousedown", () => {
+  adventureImg.classList.add("slide");
+});
+
+const destination = document.querySelectorAll(".destination h4");
+destination.forEach(dest => {
+  dest.addEventListener("click", () => {
+    console.log("clicked");
+    dest.style.color = "red";
+  });
 });
