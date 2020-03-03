fa7ccdf8efd3d450bfbc4bb56029ad440321563b
Ben Venker <bvemails@gmail.com>
2020-03-03 10:56:43 -0800

    add events to  nav, body, hero image, and let's go image

--- js/index.js
+++ js/index.js
@@ -1,1 +1,24 @@
 // Your code goes here
+const logoHeading = document.querySelector(".logo-heading");
+logoHeading.addEventListener("mouseover", e => {
+  logoHeading.style.color = "lightgray";
+});
+
+const body = document.querySelector("body");
+body.addEventListener("keydown", () => {
+  alert("How can we help you?");
+});
+
+const hero = document.querySelector(".intro img");
+hero.addEventListener("dblclick", () => {
+  hero.style.border = "2px solid blue";
+});
+
+hero.addEventListener("mouseleave", () => {
+  hero.style.border = "none";
+});
+
+const letsGoImg = document.querySelector(".img-content img");
+letsGoImg.addEventListener("load", () => {
+  letsGoImg.classList.add("change");
+});
